45222b8dd0cd4af1d70cf6306d2d8a9c44b54b7c
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-15 15:03:54 +0700

    meminta input nilai dari pengguna lalu menambahkannya ke dalam stack menggunakan fungsi push()

--- stack.cpp
+++ stack.cpp
@@ -95,6 +95,9 @@ int main()
         switch (choice)
         {
             case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value); //push the entered  value onto the stack
         }
     }
 }
